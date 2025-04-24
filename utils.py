e0ecb436a81c169a68d0c104f70b48bb2b7c7c40
paulopiui <paulovc88@gmail.com>
2025-04-24 11:43:34 -0300

    commit

--- utils.py
+++ utils.py
@@ -1,18 +1,6 @@
 import streamlit as st
 import bcrypt
 
-def exibir_cabecalho():
-    
-    col1, col2, col3 = st.columns([0.2, 0.1 ,0.6])
-
-    with col1:        
-        st.image("img/logo_preto_vermelho.png", width=300)
-
-    with col3:        
-        st.title("Gerenciamento de Projetos")
-
-    st.divider()
-
 def config_pagina():    
     st.set_page_config(
         page_title="Grupo Projeta | Projetos",      
@@ -31,6 +19,18 @@ def config_pagina():
 
     st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
+def exibir_cabecalho():
+    
+    col1, col2, col3 = st.columns([0.2, 0.1 ,0.6])
+
+    with col1:        
+        st.image("img/logo_preto_vermelho.png", width=300)
+
+    with col3:        
+        st.title("Gerenciamento de Projetos")
+
+    st.divider()
+    
 def criptografar_senha(senha):    
 
     hashed = bcrypt.hashpw(senha.encode(), bcrypt.gensalt()).decode()
